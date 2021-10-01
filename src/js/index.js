43dfa1dcff52044e1516dc8f9aa52e455e0036bf
unknown <damirsabljak@windowslive.com>
2021-10-01 10:52:03 +0200

    commit two

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 "use strict";
 const container = document.querySelector("#app");
 const pacman = document.querySelector("#pacman");
+let desigion = 0;
 const close = () => {
-  pacman.classList.remove("entity--pac");
-  pacman.classList.add("entity--pac--close");
+  if (desigion === 0) {
+    pacman.classList.remove("entity--pac");
+    pacman.classList.add("entity--pac--close");
+    desigion = 1;
+  } else if (desigion === 1) {
+    pacman.classList.remove("entity--pac--close");
+    pacman.classList.add("entity--pac");
+    desigion = 0;
+  }
 };
-container.addEventListener("click", close());
+container.addEventListener("click", close);
 console.log("it wogbvgrks!");
