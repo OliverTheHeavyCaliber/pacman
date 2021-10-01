70640d8ba0ce5ffa605c77506204b301729d4d8b
OliverTheHeavyCaliber <melicherciko@gmail.com>
2021-10-01 10:24:54 +0200

    closing mouth

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
-'use strict';
-
-console.log('it works!');
+"use strict";
+const container = document.querySelector("#app");
+const pacman = document.querySelector("#pacman");
+const close = () => {
+  pacman.classList.remove("entity--pac");
+  pacman.classList.add("entity--pac--close");
+};
+container.addEventListener("click", close());
+console.log("it wogbvgrks!");
